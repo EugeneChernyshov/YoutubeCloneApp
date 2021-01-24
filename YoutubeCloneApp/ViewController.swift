12541a8dbbebf2a77647261dca4e314cb1f5ee68
Evheniy Chernyshov <kh.410ever@gmail.com>
2021-01-24 15:57:35 +0200

    inital xcode project creation

---
+++ YoutubeCloneApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  YoutubeCloneApp
+//
+//  Created by apple on 24.01.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
